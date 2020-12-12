0625e5e838e533fbe40adbc88a8d2f5117762409
CK11 <kayaid2468@gmail.com>
2020-12-13 03:16:40 +0800

    Add Cafe Detail

---
+++ WorkingCafe/WorkingCafe/ViewControllers/CafeDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CafeDetailViewController.swift
+//  WorkingCafe
+//
+//  Created by ChristmasKay on 2020/12/9.
+//  Copyright © 2020 ChristmasKay. All rights reserved.
+//
+
+import UIKit
+
+class CafeDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ WorkingCafe/WorkingCafe/Views/Cells/CafeDetailTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CafeDetailTableViewCell.swift
+//  WorkingCafe
+//
+//  Created by ChristmasKay on 2020/12/8.
+//  Copyright © 2020 ChristmasKay. All rights reserved.
+//
+
+import UIKit
+
+class CafeDetailTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
